98cb30da382d5645ec3f15f2a5749833e1ea6ad4
Priyanshu Upadhyay <puofficialid@gmail.com>
2021-05-16 20:09:19 +0530

    Update main.py
    Removed already imported function

--- main.py
+++ main.py
@@ -6,22 +6,6 @@ from collections import OrderedDict
 from threading import Timer
 form email_alert import *
 
-def emailalert(pincode, output):
-    email_id = "sample@example.com"
-    email_pwd = "sample_password"
-
-    message = EmailMessage()
-    message['Subject'] = "Vaccine Available for 18+"
-    message['From'] = email_id
-    message['To'] = "destination_email@gmail.com"
-    message.set_content(f"Hey! vaccine for 18+ available at {pincode}\n\n{output}")
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login(email_id, email_pwd)
-        smtp.send_message(message)
-        print("Success")
-
-
-
 def extract_data():
     output=""
     today = date.today()
